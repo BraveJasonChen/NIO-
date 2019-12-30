4a2ff8f1b2a9cdae046754302a293dea56542fce
chenwei <1090921428@qq.com>
2019-12-30 20:43:55 +0800

    直接缓冲区

--- src/main/java/buffer/ByteBufferDemo.java
+++ src/main/java/buffer/ByteBufferDemo.java
@@ -1,5 +1,7 @@
 package buffer;
 
+import java.nio.ByteBuffer;
+
 /**
  * @description: 字节缓冲区demo
  * @author: brave.chen
@@ -7,10 +9,15 @@ package buffer;
  **/
 public class ByteBufferDemo {
     public static void main(String[] args) {
-
+        directBuffer();
     }
-    
+
     public static void directBuffer(){
+//分配直接缓存区
+        ByteBuffer buf = ByteBuffer.allocateDirect(1024);
+
+        //判断是否是直接缓存区
+        System.out.println(buf.isDirect());
 
     }
 }
